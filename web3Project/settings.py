feef223955aee5639dc8f2e6bd94764f2582d98c
ThomasJohnen <thomas.johnen@hotmail.be>
2023-10-30 12:04:48 +0100

    remove : idea

--- web3Project/settings.py
+++ web3Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "WishList.apps.WishlistConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
