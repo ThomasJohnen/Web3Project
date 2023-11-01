cc93317c14960c1b2aa3ae098f3220d6215e33d4
Brandon <brandon.vanbellinghen@student.vinci.be>
2023-11-01 15:34:26 +0100

    add users to Web3project settings

--- web3Project/settings.py
+++ web3Project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "WishList.apps.WishlistConfig",
+    "users.apps.UsersConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
